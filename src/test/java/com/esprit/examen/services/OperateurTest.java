271ef887c742518aec7ccab006a7e0c6f863fd89
FarjallahAhmed <ahmed.farjallah@esprit.tn>
2022-11-14 10:04:53 +0100

    test operateur

--- src/test/java/com/esprit/examen/services/OperateurTest.java
+++ src/test/java/com/esprit/examen/services/OperateurTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
